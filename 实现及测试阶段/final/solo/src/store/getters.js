fb6a78cc8fba13a6422bda53d20bd35b5740f1ee
try-alt <1545559120@qq.com>
2021-01-04 01:18:21 +0800

    121

--- 实现及测试阶段/final/solo/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-    sidebar: state => state.app.sidebar,
-    device: state => state.app.device,
-    token: state => state.user.token,
-    username: state => state.user.username
-  }
-  export default getters
